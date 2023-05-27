b41ed3fe3321bd6de63b9e3f1cad1c1281dc4150
shakeel ayaan <100689042+ShakeelAyaan@users.noreply.github.com>
2023-05-28 01:55:46 +0530

    db is upd

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 function connectDB() {
     // Database connection 🥳
-    mongoose.connect("mongodb+srv://shakeelayaan93:llwLrxNcW7KNu7FL@cluster0.6s7gtjx.mongodb.net/", { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
     const connection = mongoose.connection;
     connection.once('open', () => {
         console.log('Database connected 🥳🥳🥳🥳');
