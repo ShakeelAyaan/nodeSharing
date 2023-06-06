8cd0392fb9703adb8488579fa1eae419bbffacd7
shakeel ayaan <100689042+ShakeelAyaan@users.noreply.github.com>
2023-06-06 21:02:03 +0530

    changed url

--- server.js
+++ server.js
@@ -6,17 +6,11 @@ const path = require('path');
 const cors = require('cors');
 // Cors
 const corsOptions = {
-<<<<<<< HEAD
   origin: process.env.ALLOWED_CLIENTS.split(','),
-=======
-  origin:http://127.0.0.1:3000.split(',')
-    // origin: "http://localhost:3000,http://localhost:3300".split(',')
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
->>>>>>> 6e6c8ae42cd8958e1682cbdab61bd1f9d1d0c22a
 }
 
 app.use(cors())
-app.use(express.json());
+app.use(express.json({corsOptions}));
 
 app.use(express.static('public'));
 
@@ -33,9 +27,5 @@ app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
 
-<<<<<<< HEAD
-app.listen(PORT, console.log(`Listening on port ${PORT}.`));  
-=======
 
-app.listen(PORT, console.log(`Listening on port ${PORT}.`));
->>>>>>> 6e6c8ae42cd8958e1682cbdab61bd1f9d1d0c22a
+app.listen(PORT, console.log(`Listening on port ${PORT}.`));  
